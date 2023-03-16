798831f213852cab884b87bde9b48722a594b0ec
NidaaChok <nidaachok100@gmail.com>
2023-03-17 01:30:29 +0300

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *prints out the last part of quote in standard error
+ *Return: 1 if success
+ */
+
+int main(void)
+{
+	write(2,"and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
