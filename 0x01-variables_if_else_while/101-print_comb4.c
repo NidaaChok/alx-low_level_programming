3ffaf1c189b84e9a6b28889895d657b8ab077e8a
NidaaChok <nidaachok100@gmail.com>
2023-03-18 00:53:42 +0300

    prints all possible different combinations of three digits using putchar()

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - prints all possible different combinations of three digits
+ *Description: using putchar function in main function
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			for (k = j + 1; k <= 57; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 55 && j == 56 && k == 57)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
