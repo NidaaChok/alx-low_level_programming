21c8735b108d1f14459533d0a7f8eea02bbb2558
NidaaChok <nidaachok100@gmail.com>
2023-03-24 01:57:58 +0300

    Write a function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,16 +5,20 @@
  * @n: The integer to be printed.
  *Return: Nothing
  */
+
 void print_number(int n)
 {
+	int num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
 
-	if (n > 9)
-		print_number(n / 10);
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }
+
