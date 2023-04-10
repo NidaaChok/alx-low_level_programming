309266db207c882579c1404a3a1903a558c8c0a5
NidaaChok <nidaachok100@gmail.com>
2023-04-10 14:59:09 +0300

    Write a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,26 +12,28 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
-		printf("0\n");
-	else
+	int sum = 0;
+
+	if (argc > 1)
 	{
 		int i;
-		int sum = 0;
+		char *c;
 
 		for (i = 1; i < argc; i++)
 		{
-			if (**(argv + i) < '0' || **(argv + i) > '9')
+			for (c = argv[i]; *c != '\0'; c++)
 			{
-				printf("Error\n");
-				return (1);
+				if (*c  < '0' || *c > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-
 			sum += atoi(argv[i]);
 		}
-
-		printf("%d\n", sum);
 	}
+
+	printf("%d\n", sum);
 
 	return (0);
 }
