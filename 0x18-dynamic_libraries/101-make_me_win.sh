b2adcf3d30e60e65adf510b3f902036fca23a81a
NidaaChok <nidaaChok100@gmail.com>
2023-06-19 16:34:36 -0700

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- wget -q -O /tmp/librandy1.so https://github.com/NidaaChok/BatchScripts/raw/master/librandy1.so
- export LD_PRELOAD=/tmp/librandy1.so
+ wget -q -O /tmp/iwin.so https://github.com/NidaaChok/alx-low_level_programming/raw/master/iwin.so
+ export LD_PRELOAD=/tmp/iwin.so
